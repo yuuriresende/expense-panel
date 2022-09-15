edc60eddc3fefb41085f23104d3fe5aa9b120d18
Yuri Resende <yuuriresende@hotmail.com>
2022-09-15 08:46:04 -0300

    new button that controls to show or hide the ExpenseForm

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,5 +1,6 @@
 import './NewExpense.css'
 import ExpenseForm from '../ExpenseForm/ExpenseForm';
+import { useState } from 'react';
 const NewExpense = (props) => {
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -7,15 +8,24 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData)
+        setIsEditing(false)
     }
 
     //com os valores recebidos do componente filho, criamos uma variavel que herda todo dado digitado
     //e adiciona um id generico a ele. Ali novamente recebemos o atributo onAddExpense de appjs por props
     //enviando para appjs os valores digitados no componente filho -expenseform-
-    
+    const [isEditing, setIsEditing] = useState(false);
+
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
     return (
         <div className='new-expense'>
-           <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            { isEditing && <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} onCancel={stopEditingHandler}/>}
         </div>
     );
 }
